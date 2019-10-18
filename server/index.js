6d451bc1166a5204498a17bd384af0d03b0121fd
Jessica Huynh <huynhjessica19@gmail.com>
2019-10-17 20:40:31 -0700

    new route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-
+    res.json({
+        messsage: 'Meower!!!!'
+    });
 });
 
 app.listen(5000, () => {
