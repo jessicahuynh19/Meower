363977d85645273af59115579c22f0b7aace37cc
Jessica Huynh <huynhjessica19@gmail.com>
2019-10-17 20:23:20 -0700

    adding get route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+
+});
+
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
-});
+}); 
